76cff0099be72571be1feffe8f60cfcc8b946713
Cem <cemkgrbz@gmail.com>
2022-07-12 14:04:10 +0200

    added solutions

---
+++ solution.js
@@ -0,0 +1,32 @@
+//1
+
+const determiner = 98;
+let x;
+
+if (determiner < 0) {
+    console.log(x= 'Less than 0')
+}
+else if (determiner >= 0) {
+     console.log(x ='Greater or equal to 0')
+}
+
+//2
+
+let updater;
+let message;
+if (determiner >= 0) {
+    updater = 'Greater or equal to 0'
+    let message = 'Positive Integer'
+    console.log(message)
+}
+else {
+    updater = 'Less than 0'
+}
+
+//3
+
+
+//Ternary Operator is a programming statement. “If-Else” is a programming block. The statement is faster than Block. The Ternary Operator is not suitable for executing a function or several statements. In this case, if else is more appropriate.
+
+// const y = (x == 42) ? true : false;
+
